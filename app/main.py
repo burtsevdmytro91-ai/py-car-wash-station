849a4fcfa7e963323a99021ee53ad8ffff1231cb
Dmytro Burtsev <burtsev.dmytro91@gmail.com>
2025-11-29 23:46:56 +0100

    fix: Add all required type annotations to pass CI checks

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -12,13 +12,13 @@ class CarWashStation:
         clean_power: int,
         average_rating: float,
         count_of_ratings: int
-    ):
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def calculate_washing_price(self, car):
+    def calculate_washing_price(self, car: Car) -> float:
         cost = (
             car.comfort_class
             * (self.clean_power - car.clean_mark)
@@ -27,11 +27,11 @@ class CarWashStation:
 
         return round(cost, 1)
 
-    def wash_single_car(self, car):
+    def wash_single_car(self, car: Car) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def rate_service(self, single_rate: int):
+    def rate_service(self, single_rate: int) -> None:
         current_total_score = self.average_rating * self.count_of_ratings
         self.count_of_ratings += 1
         new_average = (
@@ -39,7 +39,7 @@ class CarWashStation:
         ) / self.count_of_ratings
         self.average_rating = round(new_average, 1)
 
-    def serve_cars(self, cars: list):
+    def serve_cars(self, cars: list) -> float:
         total_income = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
